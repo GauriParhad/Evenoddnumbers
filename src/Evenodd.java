9d6734ae41e0563832b6ae8ae76d06963cc8e1d6
Gaurip10 <thegauriparhad@gmail.com>
2021-08-08 17:26:31 +0530

    Num is even or odd

---
+++ src/Evenodd.java
@@ -0,0 +1,16 @@
+public class Evenodd {
+    public static void main(String[] args) {
+
+        int num = 13;
+
+        if (num % 2 == 0) {
+
+            System.out.println("It is Even number ");
+        } else {
+
+            System.out.println("It is Odd number");
+        }
+    }
+}
+
+
